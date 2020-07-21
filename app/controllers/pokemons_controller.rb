f00be126c01bf30a0da5cb09ae95d3927c56300f
alexpac27 <alexpac27@gmail.com>
2020-07-21 11:30:41 -0400

    destroy action working

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -4,4 +4,18 @@ class PokemonsController < ApplicationController
         pokemons = Pokemon.all
         render json: pokemons
     end
+
+    def destroy
+        # byebug
+        pokemon = Pokemon.find(params[:id])
+        pokemon.destroy
+        render json: {message: "successfully deleted"}
+    end
+
+    private
+
+    def pokemon_params
+        # pokemon.require()
+    end
+
 end
